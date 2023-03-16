997ae961a9d2d1ebf6e698b35038d00450afc586
Chase J Brignac <chase@humanish.ai>
2023-03-16 15:28:47 -0700

    eureka

--- rst_to_md.py
+++ rst_to_md.py
@@ -9,9 +9,21 @@ def copy_rst_files_to_md(src_directory):
                 dest_file = os.path.join(root, file[:-4] + '.md')
                 shutil.copy(src_file, dest_file)
                 print(f'Copied: {src_file} to {dest_file}')
+                os.remove(src_file)
+                print(f'Deleted: {src_file}')
+
+def get_total_md_size(src_directory):
+    total_size = 0
+    for root, dirs, files in os.walk(src_directory):
+        for file in files:
+            if file.endswith('.md'):
+                file_path = os.path.join(root, file)
+                total_size += os.path.getsize(file_path)
+    return total_size / (1024)  # Convert bytes to kilobytes
 
 if __name__ == "__main__":
     # Replace 'your_directory_path' with the path to the directory where you want to start searching for .rst files
     src_directory = 'Notion_DB'
     copy_rst_files_to_md(src_directory)
-
+    total_md_size = get_total_md_size(src_directory)
+    print(f'Total size of .md files in {src_directory}: {total_md_size:.2f} KB')
